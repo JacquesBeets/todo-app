4d20479d5300de5ddb8cb368624c4eac215e4921
JacquesBeets <jacques.beets@gmail.com>
2018-02-27 22:12:01 +0200

    Starting point with Toolbar and SideNav

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+    temporary 
+    absolute
+    v-model="sideNav"
+    class="hidden-md-and-up">
+      <v-list>
+        <v-list-tile v-for="menuItem in menuItems" :key="menuItem.name">
+          <v-list-tile-action>
+            <v-icon>{{menuItem.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{menuItem.name}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon 
+      @click.native.stop="sideNav = !sideNav"
+      class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Fire Pencil</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="menuItem in menuItems" :key="menuItem.name">
+          <v-icon left small>{{menuItem.icon}}</v-icon>
+          {{menuItem.name}}
+          </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        {name: 'Todos', icon: 'thumb_up'},
+        {name: 'About', icon: 'thumb_up'},
+        {name: 'Register', icon: 'thumb_up'},
+        {name: 'Sign In', icon: 'thumb_up'},
+        {name: 'Profile', icon: 'thumb_up'}
+      ]
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
